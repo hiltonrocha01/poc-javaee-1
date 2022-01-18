a2ea9ba22a50f92674d3292c5433b14fc598c31a
albertevc <albert.edmelio@cerc.inf.br>
2022-01-18 20:32:36 -0300

    Ajuste na estrutura do projeto para inicializar o jsf

--- src/main/java/br/com/aevc/mb/HelloWorldMB.java
+++ src/main/java/br/com/aevc/mb/HelloWorldMB.java
@@ -2,11 +2,14 @@ package br.com.aevc.mb;
 
 import javax.inject.Named;
 
+//import com.google.gson.Gson;
+
 @Named
 public class HelloWorldMB {
 
 	public HelloWorldMB() {
 		System.out.println("HelloWorld started!");
+//		new Gson();
 	}
 
 	public String getMessage() {
